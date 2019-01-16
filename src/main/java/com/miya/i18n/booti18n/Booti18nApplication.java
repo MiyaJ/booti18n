66cee040ddb19612401cff08a1ee6a82e0d4a9f7
蔡晓威 <540110865@qq.com>
2019-01-16 16:52:13 +0800

    新增SpringUtils   MessageUtils

--- src/main/java/com/miya/i18n/booti18n/Booti18nApplication.java
+++ src/main/java/com/miya/i18n/booti18n/Booti18nApplication.java
@@ -1,5 +1,6 @@
 package com.miya.i18n.booti18n;
 
+import com.miya.i18n.booti18n.common.MessageUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class Booti18nApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Booti18nApplication.class, args);
+        String message = MessageUtils.getMessage("mess.user.name");
+        System.out.println("message{}" + message);
     }
 
 }
---
+++ src/main/java/com/miya/i18n/booti18n/common/MessageUtils.java
@@ -0,0 +1,21 @@
+package com.miya.i18n.booti18n.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @DESC
+ * @Author:Caixiaowei
+ * @Date:2019/1/16
+ * @Time:下午4:34
+ */
+@Component
+public class MessageUtils {
+
+    public static String getMessage(String key, Object...args) {
+        MessageSource messageSource = SpringUtils.getBean(MessageSource.class);
+        String message = messageSource.getMessage(key, args, null);
+        return message;
+    }
+}
