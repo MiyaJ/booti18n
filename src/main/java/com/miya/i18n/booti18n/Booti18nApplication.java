296012d3a1d153e6944da89e7b8e1041e87ce2ef
蔡晓威 <540110865@qq.com>
2019-01-16 14:07:23 +0800

    project init

---
+++ src/main/java/com/miya/i18n/booti18n/Booti18nApplication.java
@@ -0,0 +1,14 @@
+package com.miya.i18n.booti18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booti18nApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Booti18nApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/miya/i18n/booti18n/Booti18nApplicationTests.java
@@ -0,0 +1,17 @@
+package com.miya.i18n.booti18n;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Booti18nApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
